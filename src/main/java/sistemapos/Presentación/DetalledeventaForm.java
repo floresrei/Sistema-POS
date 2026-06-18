e1b84d5f1c987ea76bbc93ad4d34dab0269c6985
nuriarodrigue <nuriarodrigue@gmail.com>
2026-06-17 21:53:38 -0600

    Cree Formulario: DetalledeventaForm -Nuria Rodriguez

---
+++ src/main/java/sistemapos/Presentación/DetalledeventaForm.java
@@ -0,0 +1,11 @@
+package sistemapos.Presentación;
+
+import javax.swing.*;
+
+public class DetalledeventaForm {
+    private JTextField textPrecioUnitario;
+    private JTextField textSubtotal;
+    private JTextField textCantidad;
+    private JTextField textProductId;
+    private JButton aceptarButton;
+}
