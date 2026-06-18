6808fe40b168ac7d7e0540843da226eb418b516d
abigail23181 <abigailrodrigxz@gmail.com>
2026-06-17 20:15:10 -0600

    Cree Formulario: ClienteForm y VentaForm-Abigail Rodriguez

---
+++ src/main/java/sistemapos/Presentación/ClienteForm.java
@@ -0,0 +1,11 @@
+package sistemapos.Presentación;
+
+import javax.swing.*;
+
+public class ClienteForm {
+    private JTextField txtNombreCliente;
+    private JTextField txtApellido;
+    private JTextField txtTelefono;
+    private JTextField txtDireccion;
+    private JButton guardarButton;
+}
---
+++ src/main/java/sistemapos/Presentación/VentaFrom.java
@@ -0,0 +1,13 @@
+package sistemapos.Presentación;
+
+import javax.swing.*;
+
+public class
+VentaFrom {
+    private JTextField textFecha;
+    private JTextField textCantidad;
+    private JTextField textTotalaPagar;
+    private JButton finalizarButton;
+    private JTextField textCliente;
+    private JTextField textProducto;
+}
